0f00c454a4faff6df79377cc1c9d4e65d4679704
Zentagon <zentagon555@hotmail.com>
2014-12-02 17:22:13 +0100

    Separerade de klasser som jag skrivit i Program.cs till nya filer

---
+++ Chess/Chess/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Menu : Board
+    {
+        // Active gör så att om användaren skriver fel så stängs inte spelet av.
+        public bool Active = true;
+        // När programmet startar så körs en liten menu som be användaren att starta spelet med kommandot "Begin". om användaren skriver fel så händer inget.
+        public void Start()
+        {
+            Console.WriteLine("Type Begin to start the automated chess game!");
+            string input = Console.ReadLine();
+            if (input == "Begin")
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+                Game = true;
+                EmptySlots();
+            }
+            else
+            {
+                Console.Clear();
+                Start();
+            }
+        }
+    }
+}
